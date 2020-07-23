25a7524fa8a72d3db4bab710482dad5c071df99f
Ryan <dsouza.ryan.11@gmail.com>
2020-07-23 19:48:50 +0530

    add tests

--- tests/app.test.ts
+++ tests/app.test.ts
@@ -16,3 +16,15 @@ test('health check', async () => {
     data: { sum: 5 },
   })
 })
+
+test('web worker computation', async () => {
+  const position = 10
+  const res = await supertest(app.server)
+    .get(`/fibo/${position}`)
+    .expect('Content-Type', /json/)
+    .expect(200)
+
+  expect(res.body).toEqual({
+    data: { position, value: 34 },
+  })
+})
